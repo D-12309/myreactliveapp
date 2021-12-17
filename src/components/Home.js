a0737e4b33bf1e929d086714be13a5b24d99f4b6
darshan <darshansavaliya2920@gmail.com>
2021-12-17 09:25:13 +0530

    my first app

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Item } from './Item'
+
+export const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row mt-3">
+                    <Item/>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Item.js
@@ -0,0 +1,116 @@
+import React from "react";
+
+export const Item = () => {
+  return (
+    <>
+      <div className="col-md-3 mt-2">
+        <div class="card">
+          <img
+            src="https://source.unsplash.com/random/"
+            style={{ height: "200px" }}
+            class="card-img-top"
+            alt="abc"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-3 mt-2">
+        <div class="card">
+          <img
+            src="https://source.unsplash.com/random/"
+            style={{ height: "200px" }}
+            class="card-img-top"
+            alt="abc"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div><div className="col-md-3 mt-2">
+        <div class="card">
+          <img
+            src="https://source.unsplash.com/random/"
+            style={{ height: "200px" }}
+            class="card-img-top"
+            alt="abc"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div><div className="col-md-3 mt-2">
+        <div class="card">
+          <img
+            src="https://source.unsplash.com/random/"
+            style={{ height: "200px" }}
+            class="card-img-top"
+            alt="abc"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div><div className="col-md-3 mt-2">
+        <div class="card">
+          <img
+            src="https://source.unsplash.com/random/"
+            style={{ height: "200px" }}
+            class="card-img-top"
+            alt="abc"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div><div className="col-md-3 mt-2">
+        <div class="card">
+          <img
+            src="https://source.unsplash.com/random/"
+            style={{ height: "200px" }}
+            class="card-img-top"
+            alt="abc"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div><div className="col-md-3 mt-2">
+        <div class="card">
+          <img
+            src="https://source.unsplash.com/random/"
+            style={{ height: "200px" }}
+            class="card-img-top"
+            alt="abc"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+    
+    </>
+  );
+};
